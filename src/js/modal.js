f84f472397425619f2ee327fb56100c65bc0c0a2
VladislavTitarenko <vladislav@1worldonline.com>
2024-02-07 23:23:33 +0200

    Refined minor styling details using BEM methodology and SCSS for enhanced maintainability and structure.

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,8 +1,10 @@
 const modal = document.querySelector('.backdrop');
 const modalBtnOpen = document.querySelector('.modal-btn-open');
+const modalBtnOpenMobile = document.querySelector('.modal-btn-open-mobile');
 const modalBtnClose = document.querySelector('.modal-btn-close');
 
 const toggleModal = () => modal.classList.toggle('is-hidden');
 
 modalBtnOpen.addEventListener('click', toggleModal);
+modalBtnOpenMobile.addEventListener('click', toggleModal);
 modalBtnClose.addEventListener('click', toggleModal);
